961cedd7a730b3a80443ce04a57855baa5b0e476
devnevo <pavansb@protonmail.com>
2018-12-28 18:22:09 +0530

    layout changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,18 @@ import Home from "./components/layout/home";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <div className="optionsLandingPage">
+          <nav>
+            <ul>
+              <li>Sign Up</li>
+              <li style={{ marginLeft: 60 }}>Log In</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
   }
 }
 
